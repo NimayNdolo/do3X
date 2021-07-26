2e73837df1452af8a8a800afaa4f1b7ea9fea84b
NimayNdolo <nemendolo@gmail.com>
2021-07-26 16:23:50 -0400

    Fix form error that ignored enter button command

--- do3x/src/App.js
+++ do3x/src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 import './App.css';
 import Todo from './Todo';
-import firebase from 'firebase';
 import db from './firebase';
 
 function App() {
@@ -17,7 +16,9 @@ function App() {
 
   const addTodo = (event) => {
     event.preventDefault();
-    db.collection('todos').add({ todo: input, timestamp: firebase.firestore.  })
+    db.collection('todos').add({ 
+      todo: input 
+    })
     setInput('');
   }
 
@@ -25,18 +26,17 @@ function App() {
     <div className="App">
       <h1>do3X</h1>
 
-      <FormControl>
-        <InputLabel >myList</InputLabel>
-        <Input value={input} onChange={event => setInput(event.target.value)} />
-      </FormControl>
-      <Button variant="contained" color="secondary" type="submit" onClick={addTodo} disabled={!input}>+</Button>
-      {/* <input value={input} onChange={event => setInput(event.target.value)}/> */}
-      {/* <button type="submit" onClick={addTodo}>+</button> */}
+    <form>
+        <FormControl>
+          <InputLabel >myList</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)} />
+        </FormControl>
+        <Button variant="contained" color="secondary" type="submit" onClick={addTodo} disabled={!input}>+</Button>
+    </form>
 
       <ul>
         {todos.map(todo => (
-          <Todo text={todo}/>
-          
+        <Todo text={todo} />
         ))}
       </ul>
     </div>
